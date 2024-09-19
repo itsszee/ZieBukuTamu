f1ba0cfce664bbf069ce4db2fd65b686fc6fd42e
itsszee <bilqistyjazilarizki@gmail.com>
2024-09-19 19:57:00 +0700

    "menambahkan modal dan fitur tambah data tamu'

--- buku-tamu.php
+++ buku-tamu.php
@@ -1,10 +1,10 @@
 <?php
-    include_once('templates/header.php');
+include_once('templates/header.php');
 ?>
 
 <?php
-    require_once('function.php');
-    include_once('templates/header.php');
+require_once('function.php');
+include_once('templates/header.php');
 ?>
 
 <!-- Custom styles for this page -->
@@ -17,11 +17,30 @@
     <!-- Page Heading -->
     <h1 class="h3 mb-4 text-gray-800">Buku Tamu</h1>
 
+    <?php
+
+    if (isset($_POST['simpan'])) {
+        if (tambah_tamu($_POST) > 0) {
+    ?>
+            <div class="alert alert-success" role="alert">
+                Data Berhasil disimpan!
+            </div>
+        <?php
+        } else {
+        ?>
+            <div class="alert alert-danger" role="alert">
+                Data gagal disimpan!
+            </div>
+    <?php
+        }
+    }
+    ?>
+
     <!-- DataTales Example -->
     <div class="card shadow mb-4">
         <div class="card-header py-3">
-            <button type="button" class="btn btn-primary btn-icon-split" 
-            data-toggle="modal" data-target="#tambahModal">
+            <button type="button" class="btn btn-primary btn-icon-split"
+                data-toggle="modal" data-target="#tambahModal">
                 <span class="icon text-white-50">
                     <i class="fas fa-plus"></i>
                 </span>
@@ -51,22 +70,23 @@
                         $no = 1;
                         // query untuk memanggil semua data daru tabel buku_tamu
                         $buku_tamu = query("SELECT * FROM bukutamu");
-                        foreach($buku_tamu as $tamu) : ?>
+                        foreach ($buku_tamu as $tamu) : ?>
 
-                        <tr>
-                            <td><?= $no++; ?></td>
-                            <td><?= $tamu['tanggal'] ?></td>
-                            <td><?= $tamu['nama_tamu'] ?></td>
-                            <td><?= $tamu['alamat'] ?></td>
-                            <td><?= $tamu['no_hp'] ?></td>
-                            <td><?= $tamu['bertemu'] ?></td>
-                            <td><?= $tamu['kepentingan'] ?></td>
-                            <td><button class="btn btn-success">Ubah</button>
-                            <button class="btn btn-danger">Hapus</button></td>
-                        </tr>
+                            <tr>
+                                <td><?= $no++; ?></td>
+                                <td><?= $tamu['tanggal'] ?></td>
+                                <td><?= $tamu['nama_tamu'] ?></td>
+                                <td><?= $tamu['alamat'] ?></td>
+                                <td><?= $tamu['no_hp'] ?></td>
+                                <td><?= $tamu['bertemu'] ?></td>
+                                <td><?= $tamu['kepentingan'] ?></td>
+                                <td><button class="btn btn-success">Ubah</button>
+                                    <button class="btn btn-danger">Hapus</button>
+                                </td>
+                            </tr>
                         <?php endforeach; ?>
                     </tbody>
-                </table>   
+                </table>
             </div>
         </div>
     </div>
@@ -74,29 +94,73 @@
 </div>
 <!-- /.container-fluid -->
 
+<?php
+$query = mysqli_query($koneksi, "SELECT max(id_tamu) as kodeTerbesar From bukutamu");
+$data = mysqli_fetch_array($query);
+$kodeTamu = $data['kodeTerbesar'];
+
+$urutan = (int) substr($kodeTamu, 2, 3);
+$urutan++;
+
+$huruf = "zt";
+$kodeTamu = $huruf . sprintf("%03s", $urutan)
+?>
+
 
 
 <!-- Modal -->
 <div class="modal fade" id="tambahModal" tabindex="-1" aria-labelledby="tambahModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="tambahModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="tambahModalLabel">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form method="post" action="">
+                    <input type="hidden" name="id_tamu" id="id_tamu" value="<?= $kodeTamu ?>">
+                    <div class="form-group row">
+                        <label for="nama_tamu" class="col-sm-3 col-form-label">Nama Tamu</label>
+                        <div class="col-sm-8">
+                            <input type="text" class="form-control" name="nama_tamu" id="nama_tamu">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="alamat" class="col-sm-3 col-form-label">Alamat</label>
+                        <div class="col-sm-8">
+                            <input type="text" class="form-control" name="alamat" id="alamat">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="no_hp" class="col-sm-3 col-form-label">No. Telepon</label>
+                        <div class="col-sm-8">
+                            <input type="text" class="form-control" name="no_hp" id="no_hp">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="bertemu" class="col-sm-3 col-form-label">Bertemu dg.</label>
+                        <div class="col-sm-8">
+                            <input type="text" class="form-control" name="bertemu" id="bertemu">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="kepentingan" class="col-sm-3 col-form-label">Kepentingan</label>
+                        <div class="col-sm-8">
+                            <input type="text" class="form-control" name="kepentingan" id="kepentingan">
+                        </div>
+                    </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary" name="simpan">Save changes</button>
+            </div>
+            </form>
+        </div>
     </div>
-  </div>
 </div>
 
 <?php
-    include_once('templates/footer.php'); 
+include_once('templates/footer.php');
 ?>
--- function.php
+++ function.php
@@ -3,7 +3,8 @@
 require_once('koneksi.php');
 
 // membuat query ke / dari database
-function query($query) {
+function query($query)
+{
     global $koneksi;
     $result = mysqli_query($koneksi, $query);
     $rows = [];
@@ -12,4 +13,24 @@ function query($query) {
     }
     return $rows;
 }
-?>
+
+function tambah_tamu($data)
+{
+
+    global $koneksi;
+
+    
+    $id_tamu        = htmlspecialchars($data["id_tamu"]);
+    $tanggal        = date("Y-m-d");
+    $nama_tamu      = htmlspecialchars($data["nama_tamu"]);
+    $alamat         = htmlspecialchars($data["alamat"]);
+    $no_hp          = htmlspecialchars($data["no_hp"]);
+    $bertemu        = htmlspecialchars($data["bertemu"]);
+    $kepentingan    = htmlspecialchars($data["kepentingan"]);
+
+    $query = "INSERT INTO bukutamu VALUES ('$id_tamu','$tanggal','$nama_tamu','$alamat','$no_hp','$bertemu','$kepentingan')";
+
+    mysqli_query($koneksi, $query);
+
+    return mysqli_affected_rows($koneksi);
+}
